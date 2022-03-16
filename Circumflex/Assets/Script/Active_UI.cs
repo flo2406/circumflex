9ac8a51c5c6d30931979a77bc826f6e3fd8d9e4e
BombRush <bombrushepita@gmail.com>
2022-03-16 18:01:38 +0100

    continuing UI of game, can activate or not the UI

---
+++ Circumflex/Assets/Script/Active_UI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Active_UI : MonoBehaviour
+{
+    [SerializeField] private GameObject UI;
+    [SerializeField] private GameObject Player;
+
+    void Start()
+    {
+        UI.active = false;
+        Player.active = true;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            if(UI.active)
+            {
+                UI.active = false;
+                Player.active = true;
+            }
+            else
+            {
+                UI.active = true;
+                Player.active = false;
+            }
+        }
+    }
+}
