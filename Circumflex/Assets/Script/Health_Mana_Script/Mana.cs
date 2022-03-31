94b22eaff2407d5de258b1ebfaa17adcdfa93dab
BombRush <bombrushepita@gmail.com>
2022-03-31 19:40:41 +0200

    Design health and mana bar

---
+++ Circumflex/Assets/Script/Health_Mana_Script/Mana.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mana : MonoBehaviour
+{
+    private Image image;
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && image.fillAmount > 0)
+            image.fillAmount -= 0.1f; 
+    }
+}
