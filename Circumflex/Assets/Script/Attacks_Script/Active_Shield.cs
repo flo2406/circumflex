d694cf76e6551922c20eaa5fd07ee99aab143a40
BombRush <bombrushepita@gmail.com>
2022-04-03 19:13:52 +0200

    creating a tornado

---
+++ Circumflex/Assets/Script/Attacks_Script/Active_Shield.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Active_Shield : MonoBehaviour
+{
+    [SerializeField] private GameObject shield;
+    private GameObject gameObject_shield;
+    void Start()
+    {
+        gameObject_shield = Instantiate(shield, transform.position, Quaternion.identity);
+    }
+
+    void Update()
+    {
+        gameObject_shield.transform.position = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
+    }
+}
