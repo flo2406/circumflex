c6628c11e5ed28934568a2d92a133071183a624c
BombRush <bombrushepita@gmail.com>
2022-06-10 12:09:17 +0200

    mana use for all attacks and boost fireball

--- Circumflex/Assets/Script/Attacks_Script/Electrical_laser.cs
+++ Circumflex/Assets/Script/Attacks_Script/Electrical_laser.cs
@@ -9,6 +9,7 @@ public class Electrical_laser : MonoBehaviour
     private KeyCode keyCode;
 
     private bool have_to_send;
+    private Gestion_Barre gestion_Barre;
 
     void Start()
     {
@@ -16,18 +17,29 @@ public class Electrical_laser : MonoBehaviour
         keyCode = KeyCode.R;
 
         have_to_send = false;
+        gestion_Barre = GameObject.FindGameObjectWithTag("barre").GetComponent<Gestion_Barre>();
     }
 
     void Update()
     {
         if (Input.GetKeyDown(keyCode))
+        {
+            gestion_Barre.use_mana(5f);
             have_to_send = true;
+        }
 
         if (Input.GetKeyUp(keyCode))
             have_to_send = false;
 
         if (have_to_send)
         {
+            bool res = gestion_Barre.use_mana(0.2f);
+            if(!res)
+            {
+                have_to_send = false;
+                return;
+            }
+
             GameObject clone = Instantiate(attack, new Vector3(transform.position.x, transform.position.y + 1.5f, transform.position.z), transform.rotation);
             clone.GetComponent<Dammages>().set_type_of_attack("electrical");
             Vector3 val = transform.TransformDirection(Vector3.forward * forceForward);
--- Circumflex/Assets/Script/Attacks_Script/Fireball.cs
+++ Circumflex/Assets/Script/Attacks_Script/Fireball.cs
@@ -7,28 +7,45 @@ public class Fireball : MonoBehaviour
     [SerializeField] GameObject attack;
     private int forceForward;
     private KeyCode keyCode;
+    private Gestion_Barre gestion_Barre;
+    private bool throwed;
 
     void Start()
     {
+        throwed = false;
         forceForward = 1000;
         //keyCode = KeyCode.A;
         keyCode = KeyCode.Q;
+        gestion_Barre = GameObject.FindGameObjectWithTag("barre").GetComponent<Gestion_Barre>();
     }
 
     void Update()
     {
-        if(Input.GetKey(keyCode))
+        if(Input.GetKey(keyCode) && (throwed || gestion_Barre.use_mana(20f)))
         {
+            throwed = true;
             gameObject.GetComponentInParent<Animations>().set_throw_anim("fireball");
         }
     }
 
-    public void send_attack()
+    public void send_ball()
     {
         GameObject clone = Instantiate(attack, new Vector3(transform.position.x, transform.position.y + 1.5f, transform.position.z), transform.rotation);
         clone.GetComponent<Dammages>().set_type_of_attack("fireball");
         Vector3 val = transform.TransformDirection(Vector3.forward * forceForward);
         clone.GetComponent<Rigidbody>().AddForce(val);
+    }
+
+
+    public void send_attack()
+    {
+        send_ball();
+        gameObject.transform.Rotate(Vector3.up, 10f);
+        send_ball();
+        gameObject.transform.Rotate(Vector3.up, -20f);
+        send_ball();
+        gameObject.transform.Rotate(Vector3.up, 10f);
+        throwed = false;
     }
 
 }
