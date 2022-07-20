91d98616a0d738d01c48c1766b68059f093790c1
BombRush <bombrushepita@gmail.com>
2022-07-20 20:08:04 +0200

    debug of number of points

--- Circumflex/Assets/Script/Stat_UI_Script/Nb_Points.cs
+++ Circumflex/Assets/Script/Stat_UI_Script/Nb_Points.cs
@@ -8,24 +8,34 @@ public class Nb_Points : MonoBehaviour
     private Text text;
     private int nb_points;
 
-    [SerializeField] private Button button_vitality;
-    [SerializeField] private Button button_wisdom;
-    [SerializeField] private Button button_strength;
-    [SerializeField] private Button button_defense;
-    [SerializeField] private Button button_speed;
-    private List<Button> arr_button;
+    [SerializeField] private GameObject button_vitality;
+    [SerializeField] private GameObject button_wisdom;
+    [SerializeField] private GameObject button_strength;
+    [SerializeField] private GameObject button_defense;
+    [SerializeField] private GameObject button_speed;
+    private List<GameObject> arr_button;
+
+    private bool hide_button;
 
     void Start()
     {
         text = GetComponent<Text>();
         nb_points = 1;
         text.text = "Points capital : 1";
-        arr_button = new List<Button>();
+        arr_button = new List<GameObject>();
         arr_button.Add(button_vitality);
         arr_button.Add(button_wisdom);
         arr_button.Add(button_strength);
         arr_button.Add(button_defense);
         arr_button.Add(button_speed);
+
+        hide_button = false;
+    }
+
+    void Update()
+    {
+        if (hide_button)
+            make_button_disable();
     }
 
     public int get_nb_points()
@@ -35,8 +45,11 @@ public class Nb_Points : MonoBehaviour
 
     public void add_nb_points(int add)
     {
-        if (nb_points == 0)
+        if (nb_points > 0)
+        {
+            hide_button = false;
             make_button_enable();
+        }
         nb_points += add;
         text.text = "Points capital : " + nb_points;
     }
@@ -45,23 +58,25 @@ public class Nb_Points : MonoBehaviour
     {
         nb_points--;
         text.text = "Points capital : " + nb_points;
+
         if (nb_points == 0)
-            make_button_disable();
+            hide_button = true;
     }
 
     public void make_button_disable()
     {
-        foreach(Button button in arr_button)
+        foreach(GameObject button in arr_button)
         {
-            button.enabled = false;
+            button.gameObject.SetActive(false);
         }
     }
     public void make_button_enable()
     {
-        foreach (Button button in arr_button)
+        foreach (GameObject button in arr_button)
         {
-            button.enabled = true;
+            button.gameObject.SetActive(true);
         }
     }
+
 
 }
