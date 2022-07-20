95ac0024dda6ab6ee4e14848dfa768c9721bc348
BombRush <bombrushepita@gmail.com>
2022-07-20 17:52:08 +0200

    lava design

---
+++ Circumflex/Assets/Script/Lava_move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava_move : MonoBehaviour
+{
+    public float scrollSpeed = 0.1f;
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float moveThis = Time.time * scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(0, moveThis));
+    }
+}
---
+++ Circumflex/Assets/Script/Move_high.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_high : MonoBehaviour
+{
+    private float y;
+    private bool activated;
+
+    private float time_between_act;
+    private float last_act;
+
+    void Start()
+    {
+        y = 68;
+        set_y();
+        activate();
+        time_between_act = 30;
+    }
+
+    void Update()
+    {
+        Debug.Log(y);
+        set_y();
+        if (activated && y < 72)
+            y += 0.003f;
+
+        else if (activated)
+            activated = false;
+
+        else if (y > 68)
+            y-= 0.003f;
+
+        //if (last_act + time_between_act < Time.time)
+            //activate();
+
+    }
+
+    private void activate()
+    {
+        activated = true;
+        y += 0.01f;
+        last_act = Time.time;
+    }
+
+    private void set_y()
+    {
+        gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, y, gameObject.transform.localPosition.z);
+    }
+}
